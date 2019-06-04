2bc6f3bebcfb029f2fb4b78e8433e05c328d5b08
David Anderson <matandersonlv@hotmail.com>
2019-06-04 17:36:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@
       def call(env)
         resp = Rack::Response.new
      
-        if (Time.now.hour >=12 and Time.now.min>=00)
+        if (Time.now.hour >=12 and Time.now.min >= 00)
           resp.write "Good Morning!"
         else
           resp.write "Good Afternoon!"
